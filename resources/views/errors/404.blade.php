254c4e41f892a49ad7838607c5a7c0344524b2cc
code-arif <arifulislam6460@gmail.com>
2026-03-09 14:44:38 +0600

    add 404 error page and 429 error page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -9,7 +9,7 @@
                 <div class="row justify-content-center">
                     <div class="col-xl-7 col-lg-8">
                         <div class="text-center">
-                            <img src="{{ asset('admin/assets/images/error400-cover.png') }}" alt="error img" class="img-fluid">
+                            <img src="{{ asset('admin/assets/images/errors/404.png') }}" alt="error img" class="img-fluid">
                             <div class="mt-3">
                                 <h3 class="text-uppercase">Sorry, Page not Found</h3>
                                 <p class="text-muted mb-4">The page you are looking for not available!</p>
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('403', function(){
+    return view('errors.404');
+});
 
 require __DIR__ . '/admin_auth.php';
