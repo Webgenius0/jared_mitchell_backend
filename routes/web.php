373470da2ed56307c4cd245782245509a9ca8602
unknown <alauddinnajim202@gmail.com>
2026-08-19 15:25:02 +0600

    manage deploy

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-require __DIR__ . '/admin_auth.php';
-
 Route::post('/webhooks/aws-ivs', [App\Http\Controllers\Api\AwsIvsWebhookController::class, 'handle'])->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
 
