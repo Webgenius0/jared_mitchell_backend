56482620ef9f019c2f84538a580e0047094e8032
unknown <alauddinnajim202@gmail.com>
2026-09-10 11:57:49 +0600

    Fix console 404/403 asset paths, JS syntax error, and duplicate storage avatar paths

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -38,17 +38,14 @@ class Profile extends Model
     public function getAvatarUrlAttribute(): string
     {
         if (! $this->avatar) {
-            return asset('admin/default/user.jpg');
+            return asset('admin/assets/images/users/user-dummy-img.jpg');
         }
 
         if (str_starts_with($this->avatar, 'http://') || str_starts_with($this->avatar, 'https://')) {
             return $this->avatar;
         }
 
-        if (str_starts_with($this->avatar, 'storage/') || str_starts_with($this->avatar, '/storage/')) {
-            return asset(ltrim($this->avatar, '/'));
-        }
-
-        return asset('storage/' . ltrim($this->avatar, '/'));
+        $cleanPath = preg_replace('#^(?:storage/|/storage/)+#i', '', $this->avatar);
+        return asset('storage/' . ltrim($cleanPath, '/'));
     }
 }
